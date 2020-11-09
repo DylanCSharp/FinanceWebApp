e5a7f13d99dfebcd6e30d75a315a43ab7f6d2d13
dylan <19005775@vcconnect.co.za>
2020-11-09 09:19:18 +0200

    Add project files.

---
+++ FinanceWebApp/Controllers/DataController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceWebApp.Controllers
+{
+    public class DataController : Controller
+    {
+        [HttpGet]
+        public IActionResult General()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult General(double income, double tax, double groceries, double waterlights, double other, double travel, double phone)
+        {
+            return RedirectToAction("Home", "Data");
+        }
+
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Home(double purchase, double deposit, int? interest, int monthsrepay)
+        {
+            return RedirectToAction("Car", "Data");
+        }
+
+        [HttpGet]
+        public IActionResult Car()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Car(string modelmake,double purchase, double deposit, int? interest, double insurance)
+        {
+            return RedirectToAction("Car", "Data");
+        }
+    }
+}
---
+++ FinanceWebApp/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FinanceWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUser(string username, string password)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
